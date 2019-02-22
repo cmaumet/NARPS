9a006a9f6ef897ac0180c4d6d00c7f078a1607f0
cmaumet <camille.maumet@inria.fr>
2019-02-22 16:38:27 +0100

    copy & gunzip func & anat files for selected subjects

--- scripts/lib/copy_gunzip.m
+++ scripts/lib/copy_gunzip.m
@@ -2,18 +2,20 @@
 % Copy to 'preproc_dir' and gunzip anatomical and fmri files found in 
 % 'study_dir' (and organised according to BIDS)
 % _________________________________________________________________________
-function copy_gunzip(study_dir, preproc_dir, varargin)
+function copy_gunzip(fmriprep_dir, preproc_dir, subject_ids)
     
-    if length(varargin) == 0 
-        sub_dirs = cellstr(spm_select('FPList', study_dir, 'dir', 'sub-*'));
+    if ~exist('subject_ids', 'var')
+        sub_dirs = cellstr(spm_select('FPList', fmriprep_dir, 'dir', 'sub-*'));
     else
-        subject_ids = varargin{1};
+        % Select only subjects of interest
         sub_dirs = cell(length(subject_ids),1);
-        for i=1:length(subject_ids)
-            sub_dirs(i,1) = cellstr(spm_select('FPList', study_dir, 'dir', sprintf('sub-%02d',subject_ids(i))));
+        for i = 1:length(subject_ids)
+            sub_dirs(i,1) = cellstr(...
+                spm_select('FPList', fmriprep_dir, 'dir', subject_ids(i)));
         end
     end
 
+    % Create subfolder to store preprocessed data in the output folder
     if ~isdir(preproc_dir)
         mkdir(preproc_dir)
     end
@@ -21,44 +23,58 @@ function copy_gunzip(study_dir, preproc_dir, varargin)
     matlabbatch = cell(0);
     for n = 1:numel(sub_dirs)
         sub_folder = sub_dirs{n};
-        anat_regexp = ['.*_T1w\.nii\.gz'];
-        fun_regexp = ['.*_bold\.nii\.gz'];
+        anat_regexp = '.*_T1w_space-MNI152NLin2009cAsym_preproc\.nii\.gz';
+        fun_regexp = '.*_bold_space-MNI152NLin2009cAsym_preproc\.nii\.gz';
 
-        amri = spm_select('FPList', fullfile(sub_folder, 'anat'), anat_regexp);
-        fmri = cellstr(spm_select('FPList', fullfile(sub_folder, 'func'), fun_regexp));
-               
-        % Copy the anatomical image
-        anat_preproc_dir = fullfile(preproc_dir, 'ANATOMICAL');
+        amri = spm_select('FPList', fullfile(sub_folder, 'anat'), ...
+                          anat_regexp);                
+       
+        % Create output anat preproc directory
+        anat_preproc_dir = fullfile(preproc_dir, 'anat');
         if ~isdir(anat_preproc_dir)
             mkdir(anat_preproc_dir)
         end        
-        matlabbatch{end+1}.cfg_basicio.file_dir.file_ops.file_move.files = {amri};
-        matlabbatch{end}.cfg_basicio.file_dir.file_ops.file_move.action.copyto = {anat_preproc_dir};
-        amri = spm_file(amri, 'path', anat_preproc_dir);
+        
+        % Copy  & gunzip the anatomical image
+        out_amri = spm_file(amri, 'path', anat_preproc_dir, 'ext', '');
+        if ~isfile(out_amri)
+            % Copy            
+            matlabbatch{end+1}.cfg_basicio.file_dir.file_ops.file_move.files ...
+                = {amri};
+            matlabbatch{end}.cfg_basicio.file_dir.file_ops.file_move.action.copyto ...
+                = {anat_preproc_dir};   
+            % Gunzip
+            matlabbatch{end+1}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.files(1) = {amri};
+            matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.outdir = {''};
+            matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.keep = false;
+        end
             
-        % For each run, copy the fmri image       
-        func_preproc_dir = fullfile(preproc_dir, 'FUNCTIONAL');
+        fmri = cellstr(...
+                    spm_select('FPList', fullfile(sub_folder, 'func'), ...
+                        fun_regexp));
+
+        % For each run, copy & gunzip the fmri image       
+        func_preproc_dir = fullfile(preproc_dir, 'func');
         if ~isdir(func_preproc_dir)
             mkdir(func_preproc_dir)
-        end                
-        for r = 1:numel(fmri)
-            matlabbatch{end+1}.cfg_basicio.file_dir.file_ops.file_move.files = {fmri{r}};
-            matlabbatch{end}.cfg_basicio.file_dir.file_ops.file_move.action.copyto = {func_preproc_dir};
-            fmri{r} = spm_file(fmri{r}, 'path', func_preproc_dir);
         end
-
-        % Gunzip anat image
-        matlabbatch{end+1}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.files(1) = {amri};
-        matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.outdir = {''};
-        matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.keep = false;
         
-        % Gunzip fmri images
+        out_fmri = cell(numel(fmri), 1);
         for r = 1:numel(fmri)
-            matlabbatch{end+1}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.files(1) = fmri(r);
-            matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.outdir = {''};
-            matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.keep = false;        
+            out_fmri{r} = spm_file(fmri{r}, 'path', func_preproc_dir, 'ext', '');
+            
+            if ~isfile(out_fmri{r})
+                % Copy
+                matlabbatch{end+1}.cfg_basicio.file_dir.file_ops.file_move.files ...
+                    = {fmri{r}};
+                matlabbatch{end}.cfg_basicio.file_dir.file_ops.file_move.action.copyto ...
+                    = {func_preproc_dir};
+                % Gunzip
+                matlabbatch{end+1}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.files(1) = fmri(r);
+                matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.outdir = {''};
+                matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.keep = false;
+            end
         end
-
     end
     spm_jobman('run', matlabbatch);    
 
--- scripts/run_w_SPM.m
+++ scripts/run_w_SPM.m
@@ -1,52 +1,60 @@
-study_dir = '/home/maullz/NIDM-Ex/BIDS_Data/DATA/BIDS/ds001_R2.0.4';
-results_dir = '/home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001';
+function run_w_SPM()
 
-spm_dir = fullfile(results_dir, 'SPM');
-if ~isdir(spm_dir)
-    mkdir(spm_dir) 
+    config()
+    
+    % Repetition time in seconds
+    TR = 1;
+    
+    raw_dir
+
+    spm_out_dir = fullfile(output_dir, 'SPM');
+    if ~isdir(spm_out_dir)
+        mkdir(spm_out_dir) 
+    end
+    
+    preproc_dir = fullfile(spm_out_dir, 'preproc');
+    level1_dir = fullfile(spm_out_dir, 'level1');
+    level2_dir = fullfile(spm_out_dir, 'level2');
+    level3_dir = fullfile(spm_out_dir, 'level3');
+
+    
+    % TODO specify dummy scans
+    % Specify the number of functional volumes ignored in the study
+    num_ignored_volumes = 0;
+    
+    % Specify the TR that will be removed from onsets, equal to num_ignored_volumes*TR
+    removed_TR_time = num_ignored_volumes*TR;
+    
+    % Add 'lib' folder to the matlab path
+    if ~exist('copy_gunzip', 'file')
+        addpath(fullfile(fileparts(mfilename('fullpath')), 'lib'))
+    end
+    
+    copy_gunzip(fmriprep_dir, preproc_dir, {'sub-001', 'sub-002', ...
+        'sub-003', 'sub-004', 'sub-005'});
+    % 
+    % % Directory to store the onset files
+    % onsetDir = fullfile(spm_out_dir,'ONSETS');
+    % 
+    % 
+    % % Define conditions and parametric modulations (if any)
+    % % FORMAT
+    % %   {VariableLabel,{TrialType,Durations}}
+    % %   {{VariableLabel,VariableModLabel},{TrialType,Duration,Amplitude}}
+    % %  
+    % CondNames = {...
+    %     {{'pumps_fixed','pumps_demean'}, {'pumps_demean', 0, 'pumps_demean'}},...
+    %     {'pumps_RT', {'pumps_demean', 'response_time'}},...
+    %     {{'cash_fixed','cash_demean'}, {'cash_demean', 0, 'cash_demean'}},...
+    %     {'cash_RT', {'cash_demean', 'response_time'}},...
+    %     {{'explode_fixed','explode_demean'}, {'explode_demean', 0, 'explode_demean'}},...
+    %     {{'control_pumps_fixed','control_pumps_demean'}, {'control_pumps_demean', 0, 'control_pumps_demean'}},...
+    %     {'control_pumps_RT', {'control_pumps_demean', 'response_time'}}};
+    % 
+    % create_onset_files(study_dir, onsetDir, CondNames, removed_TR_time);
+    % spm('defaults','FMRI');
+    % run_subject_level_analyses(study_dir, preproc_dir, 'template_ds001_SPM_level1', level1_dir, num_ignored_volumes, TR);
+    % run_group_level_analysis(level1_dir, 'template_ds001_SPM_level2', level2_dir, '0001');
+    % run_permutation_test(level1_dir, 'template_ds001_SPM_perm_test', perm_dir, '0001');
+    % mean_mni_images(preproc_dir, level1_dir, mni_dir);
 end
-
-preproc_dir = fullfile(spm_dir, 'PREPROCESSING');
-level1_dir = fullfile(spm_dir, 'LEVEL1');
-level2_dir = fullfile(spm_dir, 'LEVEL2');
-perm_dir = fullfile(level2_dir, 'permutation_test');
-mni_dir = fullfile(spm_dir, 'mean_mni_images');
-
-% Specify the number of functional volumes ignored in the study
-TR = 2;
-num_ignored_volumes = 2;
-
-% Specify the TR that will be removed from onsets, equal to num_ignored_volumes*TR
-removed_TR_time = num_ignored_volumes*TR;
-
-% Add 'lib' folder to the matlab path
-if ~exist('copy_gunzip', 'file')
-    addpath(fullfile(fileparts(mfilename('fullpath')), 'lib'))
-end
-
-copy_gunzip(study_dir, preproc_dir);
-
-% Directory to store the onset files
-onsetDir = fullfile(spm_dir,'ONSETS');
-
-
-% Define conditions and parametric modulations (if any)
-% FORMAT
-%   {VariableLabel,{TrialType,Durations}}
-%   {{VariableLabel,VariableModLabel},{TrialType,Duration,Amplitude}}
-%  
-CondNames = {...
-    {{'pumps_fixed','pumps_demean'}, {'pumps_demean', 0, 'pumps_demean'}},...
-    {'pumps_RT', {'pumps_demean', 'response_time'}},...
-    {{'cash_fixed','cash_demean'}, {'cash_demean', 0, 'cash_demean'}},...
-    {'cash_RT', {'cash_demean', 'response_time'}},...
-    {{'explode_fixed','explode_demean'}, {'explode_demean', 0, 'explode_demean'}},...
-    {{'control_pumps_fixed','control_pumps_demean'}, {'control_pumps_demean', 0, 'control_pumps_demean'}},...
-    {'control_pumps_RT', {'control_pumps_demean', 'response_time'}}};
-
-create_onset_files(study_dir, onsetDir, CondNames, removed_TR_time);
-spm('defaults','FMRI');
-run_subject_level_analyses(study_dir, preproc_dir, 'template_ds001_SPM_level1', level1_dir, num_ignored_volumes, TR);
-run_group_level_analysis(level1_dir, 'template_ds001_SPM_level2', level2_dir, '0001');
-run_permutation_test(level1_dir, 'template_ds001_SPM_perm_test', perm_dir, '0001');
-mean_mni_images(preproc_dir, level1_dir, mni_dir);
