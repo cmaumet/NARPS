e880437383e849c04a236f61e50e92fc39555257
cmaumet <camille.maumet@inria.fr>
2019-03-01 15:50:42 +0100

    group loss analysis

--- scripts/lib/run_group_level_analysis.m
+++ scripts/lib/run_group_level_analysis.m
@@ -3,52 +3,62 @@ function run_group_level_analysis(sub_names, groups, level1_dir, ...
 % (sub_names, groups, level1_dir, 'SPM_level3_gain_template', level3_dir, '0001');
 %          run_group_level_analysis(sub_names, groups, level1_dir, 'SPM_level2_template', level2_dir, '0001');
 
-    scripts_dir = fullfile(out_dir, 'batch');
-    OUT_DIR = out_dir;
+    out_file = fullfile(out_dir, 'spmT_0002.nii');
     
-    if ~isdir(scripts_dir)
-        mkdir(scripts_dir)
-    end
-    
-    group_names = unique(groups);
-    
-    CON_GROUP = {};
-    CON_GROUP{1} = {};
-    CON_GROUP{2} = {};
-    
-    for i = 1:numel(sub_names)
-        sub_dir = fullfile(level1_dir, sub_names{i});
-        [~, group_id] = ismember(groups{i}, group_names);
-            
-        CON_GROUP{group_id}{end+1,1} = spm_select(...
-            'FPList', sub_dir, ['con_' contrast_id '\.nii']);
-    end
+    if ~isfile(out_file)
+        scripts_dir = fullfile(out_dir, 'batch');
+        OUT_DIR = out_dir;
 
-    if contains(lower(group_names{1}), 'indiff')
-        CON_VECTOR_INDIFF = [1 0];
-        if contains(lower(group_names{2}), 'range')
-            CON_VECTOR_RANGE = [0 1];
-        else
-            error(['Unknown group: ' group_names{2}]);
+        if ~isdir(scripts_dir)
+            mkdir(scripts_dir)
         end
-    elseif contains(lower(group_names{1}), 'range')
-        CON_VECTOR_RANGE = [1 0];
-        if contains(lower(group_names{2}), 'indiff')
-            CON_VECTOR_INDIFF = [0 1];
-        else
-            error(['Unknown group: ' group_names{2}]);
+
+        group_names = unique(groups);
+
+        CON_GROUP = {};
+        CON_GROUP{1} = {};
+        CON_GROUP{2} = {};
+
+        for i = 1:numel(sub_names)
+            sub_dir = fullfile(level1_dir, sub_names{i});
+            [~, group_id] = ismember(groups{i}, group_names);
+
+            CON_GROUP{group_id}{end+1,1} = spm_select(...
+                'FPList', sub_dir, ['con_' contrast_id '\.nii']);
         end
-    else
-        error(['Unknown group: ' group_names{1}]);
+
+        if contains(lower(group_names{1}), 'indiff')
+            CON_VECTOR_INDIFF = [1 0];
+            CON_VECTOR_INDIFF_NEG = [-1 0];
+            if contains(lower(group_names{2}), 'range')
+                CON_VECTOR_RANGE = [0 1];
+                CON_VECTOR_RANGE_NEG = [0 -1];
+                CON_VECTOR_RANGE_VS_INDIFF = [-1 1];
+            else
+                error(['Unknown group: ' group_names{2}]);
+            end
+        elseif contains(lower(group_names{1}), 'range')
+            CON_VECTOR_RANGE = [1 0];
+            CON_VECTOR_RANGE_NEG = [-1 0];
+            if contains(lower(group_names{2}), 'indiff')
+                CON_VECTOR_INDIFF = [0 1];
+                CON_VECTOR_INDIFF_NEG = [0 -1];
+                CON_VECTOR_RANGE_VS_INDIFF = [1 -1];
+            else
+                error(['Unknown group: ' group_names{2}]);
+            end
+        else
+            error(['Unknown group: ' group_names{1}]);
+        end
+
+        % Create the matlabbatch for this subject
+        eval(group_batch_template);
+
+        save(fullfile(scripts_dir, 'group.mat'), 'matlabbatch');
+
+        % Topological FDR should be 0 for voxelwise FDR
+        global defaults
+        defaults.stats.topoFDR = 0;
+        spm_jobman('run', matlabbatch);
     end
-    
-    % Create the matlabbatch for this subject
-    eval(group_batch_template);
-        
-    save(fullfile(scripts_dir, 'group.mat'), 'matlabbatch');
-    
-    % Topological FDR should be 0 for voxelwise FDR
-    global defaults
-    defaults.stats.topoFDR = 0;
-    spm_jobman('run', matlabbatch);
 end
---
+++ scripts/templates/SPM_level3_loss_template.m
@@ -0,0 +1,55 @@
+% to define: OUT_DIR, CON_GROUP_1, CON_GROUP_2
+
+matlabbatch = {};
+matlabbatch{end+1}.spm.stats.factorial_design.dir = {OUT_DIR};
+matlabbatch{end}.spm.stats.factorial_design.des.t2.scans1 = CON_GROUP{1};
+matlabbatch{end}.spm.stats.factorial_design.des.t2.scans2 = CON_GROUP{2};
+matlabbatch{end}.spm.stats.factorial_design.des.t2.dept = 0;
+matlabbatch{end}.spm.stats.factorial_design.des.t2.variance = 1;
+matlabbatch{end}.spm.stats.factorial_design.des.t2.gmsca = 0;
+matlabbatch{end}.spm.stats.factorial_design.des.t2.ancova = 0;
+matlabbatch{end}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{end}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{end}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{end}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{end}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{end}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{end}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{end}.spm.stats.factorial_design.globalm.glonorm = 1;
+
+% estimation
+matlabbatch{end+1}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{end}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{end}.spm.stats.fmri_est.method.Classical = 1;
+
+% contrasts
+matlabbatch{end+1}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{end}.spm.stats.con.consess{1}.tcon.name = 'loss_param_indiff_neg';
+matlabbatch{end}.spm.stats.con.consess{1}.tcon.weights = CON_VECTOR_INDIFF_NEG;
+matlabbatch{end}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{end}.spm.stats.con.consess{2}.tcon.name = 'loss_param_range_neg';
+matlabbatch{end}.spm.stats.con.consess{2}.tcon.weights = CON_VECTOR_RANGE_NEG;
+matlabbatch{end}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch{end}.spm.stats.con.consess{3}.tcon.name = 'loss_param_indiff';
+matlabbatch{end}.spm.stats.con.consess{3}.tcon.weights = CON_VECTOR_INDIFF;
+matlabbatch{end}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+matlabbatch{end}.spm.stats.con.consess{4}.tcon.name = 'loss_param_range';
+matlabbatch{end}.spm.stats.con.consess{4}.tcon.weights = CON_VECTOR_RANGE;
+matlabbatch{end}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
+matlabbatch{end}.spm.stats.con.consess{5}.tcon.name = 'loss_range_vs_indiff';
+matlabbatch{end}.spm.stats.con.consess{5}.tcon.weights = CON_VECTOR_RANGE_VS_INDIFF;
+matlabbatch{end}.spm.stats.con.consess{5}.tcon.sessrep = 'none';
+
+% inference
+matlabbatch{end+1}.spm.stats.results.spmmat(1) = cfg_dep('Contrast Manager: SPM.mat File', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+for i = 1:5
+    matlabbatch{end}.spm.stats.results.conspec(i).titlestr = '';
+    matlabbatch{end}.spm.stats.results.conspec(i).contrasts = i;
+    matlabbatch{end}.spm.stats.results.conspec(i).threshdesc = 'FDR';
+    matlabbatch{end}.spm.stats.results.conspec(i).thresh = 0.05;
+    matlabbatch{end}.spm.stats.results.conspec(i).extent = 0;
+    matlabbatch{end}.spm.stats.results.conspec(i).conjunction = 1;
+    matlabbatch{end}.spm.stats.results.conspec(i).mask.none = 1;
+end
+matlabbatch{end}.spm.stats.results.units = 1;
+matlabbatch{end}.spm.stats.results.export{1}.ps = true;
